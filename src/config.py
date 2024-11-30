5d7eb5fa7a985c44013fc9a496184df0e91d497b
dbolivar25 <danielbolivarflorez@gmail.com>
2024-11-30 18:30:20 -0500

    fix config

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Final, List
 
 
@@ -8,7 +8,9 @@ class GameConfig:
 
     HEIGHT: Final[int] = 6
     WIDTH: Final[int] = 7
-    SEARCH_ORDER: Final[List[int]] = [3, 2, 4, 1, 5, 0, 6]  # Center-out search
+    SEARCH_ORDER: Final[List[int]] = field(
+        default_factory=lambda: [3, 2, 4, 1, 5, 0, 6]
+    )  # Center-out search
 
 
 @dataclass
